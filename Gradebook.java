7146023f39b59cdf282315ec917a07042b57ef64
ezamudio19 <40901737+ezamudio19@users.noreply.github.com>
2018-07-09 17:06:37 -0500

    Update Gradebook.java
    added scanner and rest of grades

--- Gradebook.java
+++ Gradebook.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class GradeBook{
   public static void main(String[] args){
-  int grade = 82;
+  int grade;
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter your grade");
+  int grade=sc.nextInt();
 /* A is a 90-100
    B is a 80-89
    C is a 70-79
@@ -8,11 +12,20 @@ public class GradeBook{
    F is less than 60
 */
     //adjust conditionals and print statements to fit the grading scale
-    if (grade >= 0 && grade < 0) {
+    if (grade >= 90 && grade < 100) {
       System.out.println("Your grade is an A");
     }
-    if (grade >= 0 && grade < 0) {
+    if (grade >= 80 && grade < 90) {
       System.out.println("Your grade is a B");
+    }
+    if (grade >= 70 && grade < 80) {
+      System.out.println("Your grade is a C");
+    }
+    if (grade >= 60 && grade < 70) {
+      System.out.println("Your grade is a D");
+    }
+    if (grade < 60) {
+      System.out.println("Your grade is a F");
     }
     // keep adding if statements to acoount for all the grade options
 
