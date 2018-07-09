1fd1db9517ee38e1f9ac04f57048d53cce00424c
ezamudio19 <40901737+ezamudio19@users.noreply.github.com>
2018-07-09 17:09:57 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
 public class GradeBook{
   public static void main(String[] args){
-  int grade;
-  Scanner sc=new Scanner(System.in);
-  System.out.println("Enter your grade");
-  int grade=sc.nextInt();
+  int grade=78;
 /* A is a 90-100
    B is a 80-89
    C is a 70-79
@@ -13,19 +9,19 @@ public class GradeBook{
 */
     //adjust conditionals and print statements to fit the grading scale
     if (grade >= 90 && grade < 100) {
-      System.out.println("Your grade is an A");
+      System.out.println("Your grade is a" +grade +" which is an A");
     }
     if (grade >= 80 && grade < 90) {
-      System.out.println("Your grade is a B");
+      System.out.println("Your grade is a" +grade +" which is an B");
     }
     if (grade >= 70 && grade < 80) {
-      System.out.println("Your grade is a C");
+      System.out.println("Your grade is a" +grade +" which is an C");
     }
     if (grade >= 60 && grade < 70) {
-      System.out.println("Your grade is a D");
+      System.out.println("Your grade is a" +grade +" which is an D");
     }
     if (grade < 60) {
-      System.out.println("Your grade is a F");
+      System.out.println("Your grade is a" +grade +" which is an F");
     }
     // keep adding if statements to acoount for all the grade options
 
